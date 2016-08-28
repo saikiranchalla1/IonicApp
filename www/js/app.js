4564b36c78b153cbd36da1eca5ad2647cbb67064
SaiKiran <saikiranchalla@gmail.com>
2016-08-28 14:23:13 -0600

    Modified Code

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic','ngSanitize'])
+angular.module('starter', ['ionic','ngSanitize', 'ServicesMod'])
 .config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
     .state('tabs', {
@@ -16,18 +16,18 @@ angular.module('starter', ['ionic','ngSanitize'])
       url: '/list',
       views: {
         'list-tab' : {
+          cache : false,
           templateUrl: 'templates/list.html',
-          controller: 'myCtrl'
+          controller: 'SportsCtrl'
         }
       }
     })
-
     .state('tabs.detail', {
       url: '/list/:feedId',
       views: {
         'list-tab' : {
           templateUrl: 'templates/detail.html',
-          controller: 'myCtrl'
+          controller: 'SportsCtrl'
         }
       }
     })
@@ -50,73 +50,22 @@ angular.module('starter', ['ionic','ngSanitize'])
     }
   });
 })
-.controller('myCtrl', function($scope, $http, $state){
+  .controller('NewsCtrl', function($scope){
+
+  })
+  .controller('ArtsCtrl', function($scope){
+
+  })
+.controller('SportsCtrl', function($scope, $http, $state, FeedFactory){
   $scope.whichFeed = $state.params.feedId;
+  $scope.platform = ionic.Platform;
   console.log("Which Feed: " + $scope.whichFeed);
-  var config = {
-                transformResponse: function (data, headers) {
-                   
-                        feeds = [];
-                        var feedsElems = angular.element(data.trim()).find("item");
-                        for (var i = 0; i < feedsElems.length; i++) {
-                            var feed = feedsElems.eq(i);
-                            var currentFeed = feed[0].getElementsByTagName('description')[0].innerText;
-                            var currentFeedObj = {
-                              index : i,
-                              description : feed[0].innerHTML.substring(1, feed[0].innerHTML.indexOf('http')-1),
-                              url : feed[0].getElementsByTagName('guid')[0].innerHTML,
-                              encoded : feed[0].getElementsByTagName('encoded')[0].innerHTML
-                            }
-                            $scope.feeds.push(currentFeedObj);
-
-                          }
-                          return $scope.feeds;
-                    } 
-                  }
-
-
-    $http.get("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D'http%3A%2F%2Fdailyutahchronicle.com%2Fcategory%2Fsports%2Ffeed%2F'&format=xml&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys", config)
-    .success(function(data){
-      console.log(data);
-       
-    })
-    .error(function(data){
-      console.log(data);
-    })
-
+  $scope.feeds = FeedFactory.get();
     $scope.init = function(){
-      var config = {
-                transformResponse: function (data, headers) {
-                   
-                        feeds = [];
-                        var feedsElems = angular.element(data.trim()).find("item");
-                        for (var i = 0; i < feedsElems.length; i++) {
-                            var feed = feedsElems.eq(i);
-                            var currentFeed = feed[0].getElementsByTagName('description')[0].innerText;
-                            var currentFeedObj = {
-                              index : i,
-                              description : currentFeed,
-                              url : feed[0].getElementsByTagName('guid')[0].innerHTML,
-                              encoded : feed[0].getElementsByTagName('encoded')[0].innerHTML
-                            }
-                            $scope.feeds.push(currentFeedObj);
 
-                          }
-                          return $scope.feeds;
-                    } 
-                  }
-
-
-    $http.get("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D'http%3A%2F%2Fdailyutahchronicle.com%2Fcategory%2Fsports%2Ffeed%2F'&format=xml&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys", config)
-    .success(function(data){
-      console.log(data);
-       $scope.whichFeed = $state.params.aId;
-    })
-    .error(function(data){
-      console.log(data);
-    })
+      $scope.feeds = FeedFactory.get();
     }
-  $scope.feeds = [];
+
   $scope.toggleStar = function(feed){
     feed.star = !feed.star;
   }
